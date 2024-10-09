02541c01b3c02fb769076f187b0087efadf81460
Ben Wainwright <bwainwright28@gmail.com>
2024-10-09 17:37:29 +0100

    Add .config/shell-files/zsh/.DS_Store Add .config/shell-files/zsh/.gitignore Add .config/shell-files/zsh/.p10k.zsh Add .config/shell-files/zsh/load-plugins.sh Add .config/shell-files/zsh/plugins.txt

---
+++ dot_config/shell-files/zsh/load-plugins.sh
@@ -0,0 +1,20 @@
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-molovo-SLASH-tipz )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-molovo-SLASH-tipz/tipz.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-docker-zsh-completion )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-greymd-SLASH-docker-zsh-completion/docker-zsh-completion.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/fancy-ctrl-z )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/fancy-ctrl-z/fancy-ctrl-z.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws/aws.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/brew )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/brew/brew.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/dircycle )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/dircycle/dircycle.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/zsh-navigation-tools )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/zsh-navigation-tools/zsh-navigation-tools.plugin.zsh
