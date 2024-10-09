f08ed3b824aa324dbe2e12f2a8f61f8197e83624
Ben Wainwright <bwainwright28@gmail.com>
2024-10-09 19:03:16 +0100

    add sudo

--- run_once_1_install_homebrew.sh
+++ run_once_1_install_homebrew.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "sudo $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
