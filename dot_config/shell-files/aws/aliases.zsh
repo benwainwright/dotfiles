9288f60b5cf36dd77fe21c18cb3294414681f64d
Ben Wainwright <bwainwright28@gmail.com>
2024-10-09 17:07:34 +0100

    Add .config/shell-files/aws/aliases.zsh Add .config/shell-files/aws/env.zsh Add .config/shell-files/aws/helper-functions.zsh

---
+++ dot_config/shell-files/aws/aliases.zsh
@@ -0,0 +1,6 @@
+alias aps='aws_cli_profile_set'
+alias as='aws-switch-profile-fzf'
+alias ecl='ecr-login'
+alias ecls='ecr-login --switch'
+alias awsdlg='aws logs describe-log-groups | jq --raw-output ".logGroups[].logGroupName"'
+alias awsdls='aws logs describe-log-streams --log-group-name'
