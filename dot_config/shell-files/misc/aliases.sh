d263bb6de97d63bb0657a108071b3c33593224d4
Ben Wainwright <bwainwright28@gmail.com>
2024-10-10 13:56:01 +0100

    Update .config/shell-files/misc/aliases.sh

--- dot_config/shell-files/misc/aliases.sh
+++ dot_config/shell-files/misc/aliases.sh
@@ -1,10 +1,10 @@
+alias ls='exa'
 alias l='ls -lFh'     #size,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias ldot='ls -ld .*'
 alias ..='cd ..'
 alias m='make'
 alias cat='bat'
-alias c='bat'
 alias ping='prettyping'
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias lop='listening-on-port'
