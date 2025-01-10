1f45dd42a94aba5d0d8fbf72978171e8180e79c0
Ben Wainwright <bwainwright28@gmail.com>
2025-01-10 20:01:20 +0000

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/lazy-bootstrap.lua

--- dot_config/nvim/lua/lazy-bootstrap.lua
+++ dot_config/nvim/lua/lazy-bootstrap.lua
@@ -1,19 +1,19 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({
-        "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo,
-        lazypath
-    })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            {"Failed to clone lazy.nvim:\n", "ErrorMsg"}, {out, "WarningMsg"},
-            {"\nPress any key to exit..."}
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({
+    "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo,
+    lazypath
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" },
+      { "\nPress any key to exit..." }
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -26,11 +26,12 @@ vim.opt.rtp:prepend(lazypath)
 -- vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-    defaults = {lazy = true, dev = {path = "~/repos"}},
-    dev = {
-      path = "~/repos"
-    },
-    spec = {{import = "plugins"}},
-    install = {colorscheme = {"habamax"}},
-    checker = {enabled = true}
+  defaults = { lazy = true, dev = { path = "~/repos" } },
+  dev = {
+    path = "~/repos"
+  },
+  spec = { { import = "plugins" } },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+  checker = { enabled = true, notify = false }
 })
